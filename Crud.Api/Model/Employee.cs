b14cca886d7483ee3dd3ce290bdc1db1097215fe
Rakibul Islam <rakibul.islam@NEXT_IT.com>
2023-01-09 17:00:12 +0600

    first commit

---
+++ Crud.Api/Model/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Api.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Number { get; set; }
+        public DateTime? Dob { get; set; }
+        public string? Gender { get; set; }
+        public string? Designation { get; set; }
+        public string? Description { get; set; }
+    }
+}
